d85cad4de4b72933975a04d3e4d729fb1e13afc0
Levianheart <andr2697@gmail.com>
2019-03-07 11:28:21 +0200

    niceblog create

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+function boot()
+{
+    Schema::defaultStringLength(191);
+}
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
